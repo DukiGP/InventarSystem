177c371042579dcbf92a6fa75f3ad0b0316b2636
DukiGP <d.d.duki@live.de>
2022-03-22 03:12:02 +0100

    oke

--- src/main/java/com/example/inventarsystem/helloController.java
+++ src/main/java/com/example/inventarsystem/helloController.java
@@ -3,13 +3,16 @@ package com.example.inventarsystem;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+final logger 
+
 @RestController
 public class helloController{
 
   // this is a Request for GET
   @RequestMapping()
   public String hello(){
-    return "Hello World";
+    int a = 2+2;
+    return "Hello World" + a;
   }
 
 }
