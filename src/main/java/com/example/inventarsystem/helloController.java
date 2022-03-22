4f5a3306e0857395d0525f37714b6c577d54df8b
DukiGP <d.d.duki@live.de>
2022-03-22 03:12:20 +0100

    nice

--- src/main/java/com/example/inventarsystem/helloController.java
+++ src/main/java/com/example/inventarsystem/helloController.java
@@ -3,8 +3,6 @@ package com.example.inventarsystem;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-final logger 
-
 @RestController
 public class helloController{
 
